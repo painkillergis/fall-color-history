9b6639c9fe2125dd389a70b1b99529b2d14a859d
tyler <tyler@cruftbusters.com>
2021-08-09 16:53:20 -0500

    extract withTestController

--- src/test/kotlin/com/painkillergis/fall_color_history/latest/LatestControllerKtTest.kt
+++ src/test/kotlin/com/painkillergis/fall_color_history/latest/LatestControllerKtTest.kt
@@ -3,17 +3,21 @@ package com.painkillergis.fall_color_history.latest
 import com.painkillergis.fall_color_history.globalModules
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
+fun <R> withTestController(controller: Application.() -> Unit, block: TestApplicationEngine.() -> R) =
+  withTestApplication({
+    globalModules()
+    controller()
+  }, block)
+
 class LatestControllerKtTest : FunSpec({
   test("latest is the same") {
-    withTestApplication({
-      globalModules()
-      latestController()
-    }) {
+    withTestController({ latestController() }) {
       handleRequest(HttpMethod.Get, "/latest").apply {
         response.status() shouldBe HttpStatusCode.OK
         Json.decodeFromString<Map<String, String>>(response.content!!) shouldBe mapOf(
