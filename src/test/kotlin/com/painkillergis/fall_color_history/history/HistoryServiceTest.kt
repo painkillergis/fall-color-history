8b15191c93cc44a0126c1f17b934caf8c4aec3fe
tyler <tyler@cruftbusters.com>
2021-08-10 14:31:37 -0500

    fixup! wip - history service returns default

--- src/test/kotlin/com/painkillergis/fall_color_history/history/HistoryServiceTest.kt
+++ src/test/kotlin/com/painkillergis/fall_color_history/history/HistoryServiceTest.kt
@@ -2,10 +2,13 @@ package com.painkillergis.fall_color_history.history
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.serialization.json.buildJsonArray
 import kotlinx.serialization.json.buildJsonObject
 
 class HistoryServiceTest : FunSpec({
   test("default history") {
-    HistoryService().get() shouldBe buildJsonObject { }
+    HistoryService().get() shouldBe buildJsonObject {
+      put("history", buildJsonArray { })
+    }
   }
 })
