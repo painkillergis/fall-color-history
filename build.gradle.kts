8848d8ef4f7173856b1776ac9118ca355858cc5e
tyler <tyler@cruftbusters.com>
2021-08-14 12:04:28 -0500

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,15 +26,18 @@ dependencies {
   implementation("io.ktor:ktor-serialization:+")
   implementation("io.ktor:ktor-server-core:+")
   implementation("io.ktor:ktor-server-netty:+")
+  implementation("org.jetbrains.exposed:exposed-core:+")
+  implementation("org.jetbrains.exposed:exposed-jdbc:+")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:+")
+  runtimeOnly("org.xerial:sqlite-jdbc:+")
   testImplementation("io.kotest:kotest-assertions-core:+")
   testImplementation("io.kotest:kotest-runner-junit5:+")
+  testImplementation("io.ktor:ktor-client-cio:+")
+  testImplementation("io.ktor:ktor-client-core:+")
   testImplementation("io.ktor:ktor-client-serialization:+")
   testImplementation("io.ktor:ktor-server-tests:+")
   testImplementation("io.mockk:mockk:+")
   testImplementation("org.jetbrains.kotlin:kotlin-test:+")
-  testImplementation("io.ktor:ktor-client-core:+")
-  testImplementation("io.ktor:ktor-client-cio:+")
 }
 
 configurations.all {
--- src/main/kotlin/com/painkillergis/fall_color_history/latest/LatestService.kt
+++ src/main/kotlin/com/painkillergis/fall_color_history/latest/LatestService.kt
@@ -2,10 +2,15 @@ package com.painkillergis.fall_color_history.latest
 
 import com.painkillergis.fall_color_history.history.HistoryService
 import kotlinx.serialization.json.JsonObject
+import org.jetbrains.exposed.sql.Database
 
 class LatestService(
   private val historyService: HistoryService,
 ) {
+  init {
+    Database.connect("jdbc:sqlite:file:fall_color_history?mode=memory&cache=shared", "org.sqlite.JDBC")
+  }
+
   private var state = emptyMap<String, Any>()
 
   fun get() = state
